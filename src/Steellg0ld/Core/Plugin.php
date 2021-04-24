80b38a450c72d994cab27c44f8d0bef8fd3d1ca6
Gaëtan H <steellgold0@gmail.com>
2021-04-24 14:15:05 +0200

    Plugin base (Wait for Twitch Stream)

---
+++ src/Steellg0ld/Core/Plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Steellg0ld\Core\Plugin;
+
+use pocketmine\plugin\PluginBase;
+
+class Plugin extends PluginBase {
+
+    public static $instance;
+
+    public function onEnable(){
+        self::$instance = $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function getInstance(){
+        return self::$instance;
+    }
+}
