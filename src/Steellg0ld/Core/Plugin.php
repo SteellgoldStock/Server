e1366b7e7c8d52de8f82c904d5939b8a28f4e8e7
Gaëtan H <steellgold0@gmail.com>
2021-04-24 17:17:47 +0200

    Main class plugin

--- src/Steellg0ld/Core/Plugin.php
+++ src/Steellg0ld/Core/Plugin.php
@@ -1,21 +1,55 @@
 <?php
 
-namespace Steellg0ld\Core\Plugin;
+namespace Steellg0ld\Core;
 
+use pocketmine\level\Position;
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use Steellg0ld\Core\data\SQL;
+use Steellg0ld\Core\games\Combat;
+use Steellg0ld\Core\listeners\LPlayers;
 
 class Plugin extends PluginBase {
+
+    const PREFIX = "§e-";
+    const BASE_COLOR = "§e";
+    const SECOND_COLOR = "§f";
+    const SERVER_NAME = "Arkanya";
 
     public static $instance;
 
     public function onEnable(){
         self::$instance = $this;
+
+        $this->getSQL()->init();
+        $this->getLogger()->critical("Database has been loaded");
+
+        $this->getServer()->getPluginManager()->registerEvents(new LPlayers(), $this);
+        $this->getLogger()->critical("Listeners loaded");
+    }
+
+    /**
+     * @return SQL
+     */
+    public function getSQL(): SQL{
+        return new SQL();
+    }
+
+    public static function getSpawn(): Position{
+        return new Position(0,256,0,Server::getInstance()->getLevelByName("world"));
     }
 
     /**
      * @return mixed
      */
-    public static function getInstance(){
+    public static function getInstance() : Plugin {
         return self::$instance;
+    }
+
+    public function getGame(String $type) : Combat{
+        switch ($type){
+            case "COMBAT":
+                return new Combat();
+        }
     }
 }
