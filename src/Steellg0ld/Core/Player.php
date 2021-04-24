376dcb675a9856fd106cadf34571671167f9dadf
Gaëtan H <steellgold0@gmail.com>
2021-04-24 22:23:52 +0200

    SQL and Data

--- src/Steellg0ld/Core/Player.php
+++ src/Steellg0ld/Core/Player.php
@@ -8,6 +8,12 @@ class Player extends \pocketmine\Player {
     public string $game = "NONE";
     public array $game_stats = [];
     public int $spell_applied = 0;
+    public int $mana = 100;
+    public bool $protego = false;
+
+    public function getStats(): array{
+        return Plugin::getInstance()->getSQL()->getPlayer($this->getName());
+    }
 
     public function getGame(): String {
         return $this->game;
@@ -18,6 +24,11 @@ class Player extends \pocketmine\Player {
     }
 
     public function dataCreation(){
-        Plugin::getInstance()->getSQL()->getDatabase()->query("INSERT INTO players VALUES ('".$this->getName()."', 0)");
+        Plugin::getInstance()->getSQL()->getDatabase()->query("INSERT INTO players VALUES ('".$this->getName()."', 0,0)");
+    }
+
+    public function getLevelByXP(Int $xp){
+        if($xp < 500) return 0;
+        return $xp / 500;
     }
 }
