3c12fcb6982c71df2f7bd700863908604ab0b44b
Gaëtan H <steellgold0@gmail.com>
2021-04-24 22:25:27 +0200

    SQL lines

--- src/Steellg0ld/Core/data/SQL.php
+++ src/Steellg0ld/Core/data/SQL.php
@@ -2,6 +2,7 @@
 
 namespace Steellg0ld\Core\data;
 
+use Steellg0ld\Core\Player;
 use Steellg0ld\Core\Plugin;
 
 class SQL {
@@ -10,14 +11,19 @@ class SQL {
     }
 
     public function init(){
-        $this->getDatabase()->query("CREATE TABLE IF NOT EXISTS players (player VARCHAR(255) PRIMARY KEY, rank INT)");
+        $this->getDatabase()->query("CREATE TABLE IF NOT EXISTS players (player TEXT, rank INT, xp INT)");
     }
 
-    public function getPlayer(): array {
+    public function getPlayer($player): array {
         $data = array();
-        while ($res = self::getDatabase()->query("SELECT * FROM players")->fetchArray(1)){
+        $query = self::getDatabase()->query("SELECT * FROM players WHERE player = '$player'");
+        while ($res = $query->fetchArray(1)){
             array_push($data,$res);
         }
         return $data[0];
+    }
+
+    public function update($whe, $col, $val)  {
+        self::getDatabase()->query("UPDATE players SET '$col' = '$val'  WHERE player = '$whe'");
     }
 }
