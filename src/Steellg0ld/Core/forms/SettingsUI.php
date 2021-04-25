18fba014df3b9e6b67de13ba26de45bc6ab84ab6
Gaëtan H <steellgold0@gmail.com>
2021-04-26 01:59:43 +0200

    Update settings (And todo)

--- src/Steellg0ld/Core/forms/SettingsUI.php
+++ src/Steellg0ld/Core/forms/SettingsUI.php
@@ -15,25 +15,34 @@ class SettingsUI
                 function (Player $p, $data) {
                     if ($data === null) {
                     } else {
-                        if ($data[3] == true){
-                            Plugin::getInstance()->getSQL()->update("players_settings",$p->getName(),"reconnect",1);
-                        }
+                        // TODO: Settings for Premium/YT/Streamer rank
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "os", $data[1]);
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "receive_messages", $data[2]);
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "reconnect", $data[3]);
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "cps", $data[4]);
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "ping", $data[5]);
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "username", $data[6]);
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "size", $data[7]);
+                        // TODO: Change skin
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "particles", $data[8]);
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "custom_os", $data[9]);
+                        Plugin::getInstance()->getSQL()->update("players_settings", $p->getName(), "pet", $data[10]);
                     }
                 }
             );
 
             $form->setTitle("- PRACTICE -");
-            $form->addLabel(Plugin::PREFIX . Plugin::SECOND_COLOR . " Bienvenu(e) sur " . Plugin::BASE_COLOR . Plugin::SERVER_NAME . Plugin::SECOND_COLOR . ", voici les paramètres disponibles sur cette interface\n");
-            $form->addToggle("Show your OS");
-            $form->addToggle("Can receive private messages");
-            $form->addToggle("Auto reconnect when server restart");
-            $form->addToggle("Show CPS");
-            $form->addToggle("Show Ping");
-            $form->addInput("Custom username");
-            $form->addSlider("Custom size", 0.5, 2);
-            $form->addSlider("Particules",0,3);
-            $form->addDropdown("Custom OS", ["Windows", "Android"]);
-            $form->addDropdown("Choose pet", ["Cat", "Dog", "Squid", "Ghast", "Wither", "Skeleton", "Zombie", "Money"]);
+            $form->addLabel(Plugin::PREFIX . Plugin::SECOND_COLOR . " Bienvenu(e) sur " . Plugin::BASE_COLOR . Plugin::SERVER_NAME . Plugin::SECOND_COLOR . ", voici les paramètres disponibles sur cette interface\n"); // 0
+            $form->addToggle("Show your OS"); // 1
+            $form->addToggle("Can receive private messages"); // 2
+            $form->addToggle("Auto reconnect when server restart", $player->getSettings()["reconnect"]); // 3
+            $form->addToggle("Show CPS"); // 4
+            $form->addToggle("Show Ping"); // 5
+            $form->addInput("Custom username"); // 6
+            $form->addSlider("Custom size", 0.5, 2); // 7
+            $form->addSlider("Particules", 0, 3); // 8
+            $form->addDropdown("Custom OS", ["Windows", "Android"]); // 9
+            $form->addDropdown("Choose pet", ["Cat", "Dog", "Squid", "Ghast", "Wither", "Skeleton", "Zombie", "Money"]); // 10
             $player->sendForm($form);
         }
     }
