0a7567b5d4c216548a02af16bd8226a76c1a690d
Gaëtan H <steellgold0@gmail.com>
2021-04-25 02:09:17 +0200

    Thx @Arcoz

--- src/Steellg0ld/Core/tasks/ManaBarTask.php
+++ src/Steellg0ld/Core/tasks/ManaBarTask.php
@@ -20,41 +20,8 @@ class ManaBarTask extends Task{
             Plugin::getInstance()->getScheduler()->cancelTask($this->getTaskId());
         }
 
-        if($this->player->mana >= 100){
-            $timeG = 10;
-            $timeR = 0;
-        }elseif($this->player->mana >= 90){
-            $timeG = 9;
-            $timeR = 1;
-        }elseif($this->player->mana >= 80){
-            $timeG = 8;
-            $timeR = 2;
-        }elseif($this->player->mana >= 70){
-            $timeG = 7;
-            $timeR = 3;
-        }elseif($this->player->mana >= 60){
-            $timeG = 6;
-            $timeR = 4;
-        }elseif($this->player->mana >= 50){
-            $timeG = 5;
-            $timeR = 5;
-        }elseif($this->player->mana >= 40){
-            $timeG = 4;
-            $timeR = 6;
-        }elseif($this->player->mana >= 30){
-            $timeG = 3;
-            $timeR = 7;
-        }elseif($this->player->mana >= 20){
-            $timeG = 2;
-            $timeR = 8;
-        }elseif($this->player->mana >= 10){
-            $timeG = 1;
-            $timeR = 9;
-        }else{
-            $timeG = 0;
-            $timeR = 10;
-        }
-
-        $this->player->sendTip(str_repeat("§a⬛", $timeG) . str_repeat("§c⬛", $timeR));
+        $timeG = floor($this->player->mana / 10);
+        $timeR = 10 - $timeG;
+        $this->player->sendTip(str_repeat(Plugin::MANA. "⬛", $timeG) . str_repeat("§7⬛", $timeR));
     }
 }
