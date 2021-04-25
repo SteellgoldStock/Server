1aada4434f55c841cc9655374fab30120efde666
Gaëtan H <steellgold0@gmail.com>
2021-04-25 02:17:16 +0200

    Thanks @arclegrandroi for opti

---
+++ src/Steellg0ld/Core/tasks/ManaBarTask.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Steellg0ld\Core\tasks;
+
+use pocketmine\scheduler\Task;
+use pocketmine\Server;
+use Steellg0ld\Core\Player;
+use Steellg0ld\Core\Plugin;
+
+class ManaBarTask extends Task{
+
+    public $player;
+
+    public function __construct(Player $player){
+        $this->player = $player;
+    }
+
+    public function onRun(int $currentTick){
+        if($this->player->getLevel() !== Server::getInstance()->getLevelByName("world") OR $this->player->game == "NONE"){
+            Plugin::getInstance()->getScheduler()->cancelTask($this->getTaskId());
+        }
+
+        $timeG = floor($this->player->mana / 10);
+        $timeR = 10 - $timeG;
+        $this->player->sendTip(str_repeat(Plugin::MANA. "⬛", $timeG) . str_repeat("§7⬛", $timeR));
+    }
+}
