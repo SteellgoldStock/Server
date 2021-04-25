000a7ad667c693d87916719f504c0b8b4cec6a2a
Gaëtan H <steellgold0@gmail.com>
2021-04-25 02:09:25 +0200

    Add Soon Feature

---
+++ src/Steellg0ld/Core/forms/SettingsUI.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Steellg0ld\Core\forms;
+
+use jojoe77777\FormAPI\CustomForm;
+use Steellg0ld\Core\Player;
+use Steellg0ld\Core\Plugin;
+
+class SettingsUI
+{
+    public static function openCompassUI(Player $player)
+    {
+        {
+            $form = new CustomForm(
+                function (Player $p, $data) {
+                    if ($data === null) {
+                    } else {
+                        if ($data[3] == true){
+                            Plugin::getInstance()->getSQL()->update($p->getName(),"settings");
+                        }
+                    }
+                }
+            );
+
+            $form->setTitle("- PRACTICE -");
+            $form->addLabel(Plugin::PREFIX . Plugin::SECOND_COLOR . " Bienvenu(e) sur " . Plugin::BASE_COLOR . Plugin::SERVER_NAME . Plugin::SECOND_COLOR . ", voici les paramètres disponibles sur cette interface\n");
+            $form->addToggle("Show your OS");
+            $form->addToggle("Can receive private messages");
+            $form->addToggle("Auto reconnect when server restart");
+            $form->addToggle("Show CPS");
+            $form->addToggle("Show Ping");
+            $form->addInput("Custom username");
+            $form->addSlider("Custom size", 0.5, 2);
+            $form->addSlider("Particules",0,3);
+            $form->addDropdown("Custom OS", ["Windows", "Android"]);
+            $form->addDropdown("Choose pet", ["Cat", "Dog", "Squid", "Ghast", "Wither", "Skeleton", "Zombie", "Money"]);
+            $player->sendForm($form);
+        }
+    }
+}
